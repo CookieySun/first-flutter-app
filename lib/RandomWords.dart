4d170a55e9b46eee9b78bfdd012a23eecd5b8069
kktyu <ku.k669500@gmail.com>
2021-05-31 22:20:28 +0900

    ファイルを分割

---
+++ lib/RandomWords.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '_RandomWords.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
